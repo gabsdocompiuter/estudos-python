923cf67225b362dcc2798fc625b702c633541d02
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-11-29 11:15:15 -0300

    Cria outros métodos

--- REST APIs with Flask and Python/jwt/app.py
+++ REST APIs with Flask and Python/jwt/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -14,6 +14,14 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help='Price is mandatory'
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x.get('name') == name, items), None)
@@ -24,11 +32,29 @@ class Item(Resource):
         if next(filter(lambda x: x.get('name') == name, items), None):
             return {'message', 'This name already be used'}, 400
 
-        req = request.get_json()
+        req = Item.parser.parse_args()
 
         item = {'name': name, 'price': req.get('price')}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x.get('name') != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        req = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x.get('name') == name, items), None)
+
+        if item is None:
+            item = {'name': name, 'price': req.get('price')}
+            items.append(item)
+        else:
+            item.update(req)
+
+        return item
 
 
 class ItemList(Resource):
@@ -39,4 +65,5 @@ class ItemList(Resource):
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
-app.run()
+if __name__ == '__main__':
+    app.run()
